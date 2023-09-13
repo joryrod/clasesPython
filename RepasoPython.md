a6818e8539a8c01d9dd76757377a6b9ba4319079
Joryrod <joryrodriguez25@gmail.com>
2023-09-13 12:45:49 -0500

    clases

---
+++ RepasoPython.md
@@ -0,0 +1,7 @@
+#Repaso Python#
+## 1. tipos de datos##
+## 2. variables
+## 3. operadores
+## 4. datos estructurados
+## 5. funciones
+## 6. controles de flujo
---
+++ main.py
@@ -0,0 +1,43 @@
+##                DATOS
+# - "a"string cadena de texto
+# - "hola" esto tambien es un string
+# -"hola soy un string y te saludo" cadena larga
+# *OBSERVACION: todo lo que este entre comillas es un string
+# "100"
+# "falce"
+# "hola"
+# # un string puede estar entre comillas dobles o simples 
+# - 100 este es un tipo de dato numerico entero integer
+# - 2.1 estees un tipo de dato numerico de tipo flotante float
+# - falce este es un tipo de dato boleano falso
+# - true tipo de dato boleano verdadero
+
+##                VARIABLES
+# es donde almacenamos nuestros tipos de datos
+# esos datos pueden mutar cambiar o modificarce
+# - como creamos una variable para almacenar nuestros datos?
+# 1. darle un nombre significativo o relacionado al dato que estamos almacenando
+# 2. asignarle a que dato esta asociado(proceso de asignacion) con el simbolo "="
+# 3. indicar el tipo de dato que se va almacenar -> darle el dato a guardar
+
+##                 OPERADORES
+# 1. operadores aritmeticos:
+# - suma "+"
+# - resta "-"
+# - multiplicacion "*"
+# - divicion "/"
+# op=15+12+14+13/4 precedencia de operadores
+# suma=45+42 operador suma resultado 87
+# suma="45"+12 operador de concatenacion resultado 4512
+# saludo="hola"+"mundo" concatenacion holamundo
+# saludo2="hola"+" "+"mundo" concatenacion hola mundo
+# saludos="hola"*2 holahola
+
+##                  DATOS ESTRUCTURADOS
+# 1. listas: puede almacenar distintos tipos de datos en una lista separados por comas
+# lista=["nombres",10,falce]
+# 2. objetos: al igual que las listas almacenan distintos tipos de datos pero con un orden. 
+# para almacenar datos en un objeto necesitamos especificar un indice y un valor clave-> valor  
+# alumno={"nombre":"jory","edad:":"18","sexo":"todos los dias"}
+# - combinar ambas estructuras de datos
+# alumno={"nombre":"jory","edad":30,"amigos":["anthony","edwin","china"],"direcion":{"departamento":"ayacucho","provincia":"lucanas","distrito":"puquio"}}
